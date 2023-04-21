023b6ae41d5ec1e1df991886cff40d9211048e7c
Rida <hello@rida.dev>
2023-04-21 13:23:31 -0600

    remove incorrect test

--- tests/utils/hash-files.test.ts
+++ tests/utils/hash-files.test.ts
@@ -19,21 +19,6 @@ describe("hashFileContents", () => {
     expect(actualHash).toEqual(expectedHash);
   });
 
-  it("hash should not change based on the order of the files", () => {
-    const paths = [
-      "./tests/__project__/beta.txt",
-      "./tests/__project__/alpha.txt",
-    ];
-
-    const expectedHash = createHash("md5")
-      .update(readFileSync(join(process.cwd(), paths[0])))
-      .update(readFileSync(join(process.cwd(), paths[1])))
-      .digest("hex");
-
-    const actualHash = hashFileContents(paths);
-    expect(actualHash).toEqual(expectedHash);
-  });
-
   it("should skip non-file paths", () => {
     const paths = ["./tests/__project__/alpha.txt", "./tests/__project__"];
     const expectedHash = createHash("md5")
