b3dc9691112d121b2021357cf81dace66dbb308e
Rida <hello@rida.dev>
2023-04-21 19:48:25 -0600

    fixup! add ability to select wasm xxhash for better performance

--- tests/utils/hash-dependencies.test.ts
+++ tests/utils/hash-dependencies.test.ts
@@ -13,8 +13,19 @@ describe("hashFileContents", () => {
     expect(hash).toBeTypeOf("string");
   });
 
-  it("should always generate the same hash", () => {
+  it("should always generate the same hash (sha1)", () => {
     const { hash } = hashDependencies({
+      algorithm: "sha1",
+      rootDirectory: join(process.cwd(), "tests", "__project__"),
+      additionalPatterns: ["**/*.txt"],
+    });
+
+    expect(hash).toMatchSnapshot();
+  });
+
+  it("should always generate the same hash (xxhash)", () => {
+    const { hash } = hashDependencies({
+      algorithm: "xxhash",
       rootDirectory: join(process.cwd(), "tests", "__project__"),
       additionalPatterns: ["**/*.txt"],
     });
